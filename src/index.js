8e88ff17571bb11ef486abeaaeb284f710e42886
Aftab H <aftabh.work@gmail.com>
2018-03-17 20:59:01 -0400

    Add webpack HMR support.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { AppContainer } from 'react-hot-loader';
 import ReactDOM from "react-dom";
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const render = Component => {
+  const MainApp = (
+    <AppContainer>
+      <Component />
+    </AppContainer>
+  );
+
+  ReactDOM.render(MainApp, document.getElementById("app"));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App.js', () => {
+    render(require('./components/App'));
+  });
+}
