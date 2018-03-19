9469a781e1b5f7a3c5f91c298a2d483021f7a59a
Aftab H <aftabh.work@gmail.com>
2018-03-18 21:07:24 -0400

    Update component and add a simple counter view.

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,10 +4,47 @@ import './style.scss';
 
 
 class Content extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        const initialState = {
+            value: 0,
+        };
+
+        this.state = initialState;
+    }
+
+    onIncrement() {
+        const newState = {
+            value: this.state.value + 1
+        };
+
+        this.setState(newState);
+    }
+
+    onDecrement() {
+        const newState = {
+            value: this.state.value - 1
+        };
+
+        this.setState(newState);
+    }
+
     render() {
+        const value = this.state.value;
+
         return (
             <div className='content'>
-                Hello world!!!
+                <span>Counter: {value}</span>
+
+                <button onClick={() => this.onIncrement()}>
+                    increment
+                </button>
+
+                <button onClick={() => this.onDecrement()}>
+                    decrement
+                </button>
             </div>
         );
     }
