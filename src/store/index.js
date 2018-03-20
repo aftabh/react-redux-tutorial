df07a2f9700f3b6b779eea98325b0fe72a22982b
Aftab H <aftabh.work@gmail.com>
2018-03-20 13:47:43 -0400

    Add a Redux middleware for logging actions.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducers from './reducers';
 
-export default createStore(reducers);
+const Logger = store => next => action => {
+    const LOG_GROUP = 'Log: ' + (new Date()).toString();
+
+    console.group(LOG_GROUP);
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd(LOG_GROUP);
+
+    return result;
+}
+
+export default createStore(reducers, applyMiddleware(Logger));
