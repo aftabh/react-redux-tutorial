6e77b1d1679338c88621011a022899e225dd40f6
Aftab H <aftabh.work@gmail.com>
2018-03-20 08:52:27 -0400

    Reduxify component state management code.

--- src/components/Content/container.js
+++ src/components/Content/container.js
@@ -1,32 +1,53 @@
 import React from "react";
 import ContentView from './component';
 
+
+const initialState = {
+    value: 0,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                value: state.value + 1
+            };
+
+        case 'DECREMENT':
+            return {
+                value: state.value - 1
+            };
+
+        default:
+            return state;
+    }
+};
+
+
 class ContentContainer extends React.Component {
 
     constructor(props) {
         super(props);
 
-        const initialState = {
-            value: 0,
-        };
+        const initialState = reducer(undefined, {});
 
         this.state = initialState;
     }
 
-    onIncrement() {
-        const newState = {
-            value: this.state.value + 1
-        };
+    dispatch(action) {
+        this.setState(prevState => reducer(prevState, action));
+    }
 
-        this.setState(newState);
+    onIncrement() {
+        this.dispatch( /* action */ {
+            type: 'INCREMENT'
+        });
     }
 
     onDecrement() {
-        const newState = {
-            value: this.state.value - 1
-        };
-
-        this.setState(newState);
+        this.dispatch( /* action */ {
+        	type: 'DECREMENT' 
+        });
     }
 
     render() {
